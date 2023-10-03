8b5af7253c5b4078abcb9b4426736daabb9d2832
gus17 <gustavopantoja.ap@gmail.com>
2023-10-03 13:42:26 -0300

    correcao de url

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect()->route('dashboard');
-});
 Route::get('home',function (){
     return redirect(url('perfil'));
 });
+
+Route::get('/', function () {
+    return redirect()->route('dashboard');
+});
+
 
 Route::get('/register', function () {
     return redirect()->route('dashboard');
