762743b3f70af3db54e4d0662b54e0ed53ba6813
Gustavo Pantoja <gustavopantoja.ap@gmail.com>
2023-10-03 11:40:38 -0300

    inicial

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,8 +7,8 @@
     <meta charset="utf-8"/>
     <title>GIFTLOVES | SiteLogista</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description"/>
-    <meta content="Themesbrand" name="author"/>
+    <meta content="gifrloves sistema de apoio ao logista" name="description"/>
+    <meta content="GIFTLOVES" name="author"/>
     <!-- App favicon -->
     <link rel="shortcut icon" href="assets/images/favicon.ico">
 
