9b60c38c4b0a5e22e8e0d784c9ae2c6d552f5efa
gus <gustavopantoja.ap@gmail.com>
2023-12-16 13:00:43 -0300

    primeiro acesso

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -48,10 +48,7 @@ class LoginController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
+
 
 
 }
